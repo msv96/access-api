7b379744d396d4845fd9a8c56693f3909d4651bd
Srinivasan Mohan <srinivasan.m@leanfolks.com>
2022-08-24 12:27:15 +0530

    report api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-
-const indexRouter = require('./routes/index');
+import express, { json } from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+import { evaluate } from 'aatt';
 
 const app = express();
 const port = process.env.PORT || 3004;
@@ -11,8 +11,31 @@ app.use(
     origin: '*',
   })
 );
-app.use(express.json());
-app.use('/api/v1', indexRouter);
+app.use(json());
+
+app.get('/api/v1/report', async function (req, res) {
+  const { url } = req.query;
+  try {
+    if (url) {
+      const html = await fetch(url);
+      const doc = await html.text();
+      const data = await evaluate({
+        source: doc,
+        output: 'json',
+        engine: 'htmlcs',
+        level: 'WCAG2A',
+      });
+      const result = data.includes('Object]\n')
+        ? data?.split('Object]\n')[1]
+        : data;
+      res.status(200).json({ status: true, data: JSON.parse(result) });
+    } else {
+      res.status(404).json({ status: false, message: 'URL is required' });
+    }
+  } catch (error) {
+    res.status(500).json({ status: false, message: error });
+  }
+});
 
 app.listen(port, function (req, res) {
   console.log('server:', port);
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/report', function (req, res) {
-  const { url } = req.query;
-  res.json({ msg: 'hi', url });
-});
-
-module.exports = router;
