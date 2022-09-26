4a75cc6526b4eeb13b44c2337f1feed80ae0fb50
Srinivasan Mohan <srinivasan.m@leanfolks.com>
2022-09-26 21:25:00 +0530

    pdf checker added

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express, { json } from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
 import { evaluate } from 'aatt';
+import checkPdf from 'wcag-pdf';
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3005;
 
 app.use(
   cors({
-    origin: '*',
+    origin: '*'
   })
 );
 app.use(json());
@@ -23,14 +24,35 @@ app.get('/api/v1/report', async function (req, res) {
         source: doc,
         output: 'json',
         engine: 'htmlcs',
-        level: 'WCAG2A',
+        level: 'WCAG2A'
       });
       const result = final.includes('Object]\n')
         ? final?.split('Object]\n')[1]
         : final;
-      res.status(200).json({ status: true, data: JSON.parse(result), date: new Date().toLocaleString() });
+      res
+        .status(200)
+        .json({
+          status: true,
+          data: JSON.parse(result),
+          date: new Date().toLocaleString()
+        });
     } else {
       res.status(404).json({ status: false, message: 'URL is required' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: false, message: error });
+  }
+});
+
+app.get('/api/v1/pdf-report', async function (req, res) {
+  const { url } = req.query;
+  try {
+    if (url) {
+      const result = await checkPdf(url);
+      res.status(200).json({ status: true, data: result });
+    } else {
+      res.status(404).json({ status: false, message: 'PDF link or pdf file is required' });
     }
   } catch (error) {
     console.log(error);
