f12da27cabd48f7513fb65d355c9ead71d318a3a
Srinivasan Mohan <srinivasan.m@leanfolks.com>
2022-09-27 12:23:56 +0530

    multer added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch';
-import { evaluate } from 'aatt';
-import checkPdf from 'wcag-pdf';
+const express = require('express');
+const cors = require('cors');
+const fetch = (...args) =>
+  import('node-fetch').then((mod) => mod.default(...args));
+const { evaluate } = require('aatt');
+const checkPdf = require('wcag-pdf');
+const multer = require('multer');
+const { unlink } = require('node:fs');
 
 const app = express();
 const port = process.env.PORT || 3005;
@@ -12,7 +15,8 @@ app.use(
     origin: '*'
   })
 );
-app.use(json());
+app.use(express.json());
+app.use('/tmp', express.static(__dirname + '/tmp'));
 
 app.get('/api/v1/report', async function (req, res) {
   const { url } = req.query;
@@ -29,13 +33,11 @@ app.get('/api/v1/report', async function (req, res) {
       const result = final.includes('Object]\n')
         ? final?.split('Object]\n')[1]
         : final;
-      res
-        .status(200)
-        .json({
-          status: true,
-          data: JSON.parse(result),
-          date: new Date().toLocaleString()
-        });
+      res.status(200).json({
+        status: true,
+        data: JSON.parse(result),
+        date: new Date().toLocaleString()
+      });
     } else {
       res.status(404).json({ status: false, message: 'URL is required' });
     }
@@ -52,7 +54,7 @@ app.get('/api/v1/pdf-report', async function (req, res) {
       const result = await checkPdf(url);
       res.status(200).json({ status: true, data: result });
     } else {
-      res.status(404).json({ status: false, message: 'PDF link or pdf file is required' });
+      res.status(404).json({ status: false, message: 'PDF link is required' });
     }
   } catch (error) {
     console.log(error);
@@ -60,6 +62,70 @@ app.get('/api/v1/pdf-report', async function (req, res) {
   }
 });
 
-app.listen(port, function (req, res) {
+app.post('/api/v1/pdf-file-report', async function (req, res) {
+  try {
+    const upload = multer({
+      storage: multer.diskStorage({
+        destination: 'tmp/',
+        filename: function (_req, file, cb) {
+          return cb(null, file.originalname.replace(' ', '_'));
+        }
+      }),
+      limits: {
+        fileSize: 5242880,
+        files: 1
+      },
+      fileFilter: function (_req, file, cb) {
+        const type = file.originalname.split('.');
+        const ext = type[type.length - 1];
+        if (ext !== 'pdf') {
+          return cb(new Error(`Only pdf file is allowed`));
+        }
+        return cb(null, true);
+      }
+    }).single('myPdf');
+    upload(req, res, async function (err) {
+      try {
+        if (err) {
+          return res.status(400).send({
+            error: err,
+            message: 'Bad Request',
+            status: false
+          });
+        } else {
+          if (!req.file) {
+            return res
+              .status(404)
+              .send({ status: false, message: 'File not found' });
+          }
+          const host = req.headers.host;
+          const url = host.includes('localhost') ? `http://${host}/` : `https://${host}/`;
+          const result = await checkPdf(url + req.file.path);
+          unlink(req.file.path, (err) => {
+            if (err) throw new Error(err);
+            console.log(req.file.path + ' was deleted');
+          });
+          return res.status(200).send({ status: true, file: req.file, result });
+        }
+      } catch (error) {
+        unlink(req.file.path, (err) => {
+          if (err) throw new Error(err);
+          console.log(req.file.path + ' was deleted');
+        });
+        console.log(error);
+        return res.status(404).send({ status: false, message: error });
+      }
+    });
+  } catch (error) {
+    unlink(req.file.path, (err) => {
+      if (err) throw new Error(err);
+      console.log(req.file.path + ' was deleted');
+    });
+    console.log(error);
+    return res.status(500).send({ status: false, message: error });
+  }
+});
+
+app.listen(port, function (_req, _res) {
   console.log('server:', port);
 });
